d800e3774090010633ffb38f82dcf00d19b3d74b
Caio <77120914+cesadev@users.noreply.github.com>
2026-09-07 16:41:39 -0300

    Itens alterations

---
+++ scripts/item/ItemData.gd
@@ -0,0 +1,6 @@
+class_name ItemData
+extends Resource
+
+@export var nome: String = "Item"
+@export var valor: int = 10
+@export var icone: Texture2D # Opcional: já deixa pronto para quando for colocar na UI
